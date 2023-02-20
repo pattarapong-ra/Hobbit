f94ca1ef9383788081f52c605ca76e0176cfc6ce
pattarapong-ra <pattarapong.ra@tnis.com>
2023-02-20 11:52:37 +0700

    แก้บั๊กใส่ text ลงช่องที่รับตัวเลข

--- exercise2/number2.go
+++ exercise2/number2.go
@@ -13,7 +13,12 @@ func ShowProduct(p []Product) {
 	fmt.Println("**************************")
 	fmt.Println("รายการสินค้า")
 	for _, val := range p {
-		fmt.Printf("รหัสสินค้า : %d\nชื่อสินค้า : %s\nราคาสินค้า : %d\nจำนวนสินค้า : %d\n\n", val.productID, val.productName, val.productPrice, val.productQuantity)
+		if val.productQuantity == 0 {
+			fmt.Printf("รหัสสินค้า : %d\nชื่อสินค้า : %s\nราคาสินค้า : %d\nจำนวนสินค้า : สินค้าหมด\n\n", val.productID, val.productName, val.productPrice)
+		} else {
+			fmt.Printf("รหัสสินค้า : %d\nชื่อสินค้า : %s\nราคาสินค้า : %d\nจำนวนสินค้า : %d\n\n", val.productID, val.productName, val.productPrice, val.productQuantity)
+		}
+
 	}
 	fmt.Println("**************************")
 }
@@ -21,6 +26,7 @@ func ShowProduct(p []Product) {
 func main() {
 	productList := []Product{}
 	menuChoice := ""
+	var existed bool
 
 	ShowProduct(productList)
 	for menuChoice != "exit" {
@@ -38,24 +44,57 @@ func main() {
 			var productChoice, buyQuantity, sumPrice, payMoney, change int
 			var changeThousand, changeFiveHundred, changeOneHundred, changeFifty, changeTwenty, changeTen, changeFive, changeOne int
 
-			fmt.Printf("เลือกชนิดสินค้า (พิมพ์ ID) :")
+			fmt.Printf("เลือกชนิดสินค้า (พิมพ์ ID)(กด -1 เพื่อยกเลิกการซื้อ) :")
 			fmt.Scanf("%d", &productChoice)
-			fmt.Printf("จำนวนสินค้าที่ซื้อ :")
+			if productChoice == -1 {
+				break
+			}
+			for _, val := range productList {
+				if productChoice == val.productID {
+					existed = true
+					break
+				}
+			}
+			for existed != true {
+				fmt.Println("ไม่มีสินค้านี้ในระบบ กรุณากรอกรหัสใหม่อีกครั้ง")
+				fmt.Printf("เลือกชนิดสินค้า (พิมพ์ ID) :")
+				fmt.Scanf("%d", &productChoice)
+				for _, val := range productList {
+					if productChoice == val.productID {
+						existed = true
+					}
+				}
+				if existed == true {
+					break
+				}
+			}
+
+			fmt.Printf("จำนวนสินค้าที่ซื้อ (กด -1 เพื่อยกเลิกการซื้อ) :")
 			fmt.Scanf("%d", &buyQuantity)
-			for productList[productChoice].productQuantity < buyQuantity {
-				fmt.Println("สินค้าที่เลือกมีจำนวนไม่พอ กรุณาลองใหม่อีกครั้ง")
+			if buyQuantity == -1 {
+				break
+			}
+			for productList[productChoice-1].productQuantity < buyQuantity || buyQuantity == 0 {
+				if buyQuantity == -1 {
+					break
+				}
+				fmt.Println("จำนวนสินค้าไม่ถูกต้อง กรุณาลองใหม่อีกครั้ง")
 				fmt.Printf("จำนวนสินค้าที่ซื้อ :")
 				fmt.Scanf("%d", &buyQuantity)
-			} //if not enough product
-			sumPrice = productList[productChoice].productPrice * buyQuantity
+			}
+			//if not enough product
+			sumPrice = productList[productChoice-1].productPrice * buyQuantity
 			fmt.Printf("ราคารวม : %d\n", sumPrice)
-			fmt.Printf("จำนวนเงินที่จ่าย :")
+			fmt.Printf("จำนวนเงินที่จ่าย (กด -1 เพื่อยกเลิกการซื้อ):")
 			fmt.Scanf("%d", &payMoney)
+			if payMoney == -1 {
+				break
+			}
 			if sumPrice > payMoney {
 				fmt.Println("จำนวนเงินที่จ่ายไม่เพียงพอ ทำรายการไม่สำเร็จ")
 				fmt.Println("กรุณาทำรายการใหม่อีกครั้ง")
 			} else {
-				productList[productChoice].productQuantity -= buyQuantity
+				productList[productChoice-1].productQuantity -= buyQuantity
 				fmt.Println("ทำรายการสำเร็จ")
 			}
 			//if enough money proceed
@@ -91,7 +130,6 @@ func main() {
 			}
 			ShowProduct(productList)
 			//print ทอนกี่แบ๊ง กี่เหรียญ
-
 		case "add":
 
 			var product Product
@@ -100,16 +138,25 @@ func main() {
 			var itemIndex int
 			dupe := false
 
-			addProductID = len(productList)
+			addProductID = len(productList) + 1
 			product.productID = addProductID
-			fmt.Printf("ชื่อของสินค้าที่เพิ่ม : ")
+			fmt.Printf("ชื่อของสินค้าที่เพิ่ม (กด -1 เพื่อยกเลิกการซื้อ): ")
 			fmt.Scanf("%s", &addProductName)
+			if addProductName == "-1" {
+				break
+			}
 			product.productName = addProductName
-			fmt.Printf("ราคาของสินค้าที่เพิ่ม : ")
+			fmt.Printf("ราคาของสินค้าที่เพิ่ม (กด -1 เพื่อยกเลิกการซื้อ): ")
 			fmt.Scanf("%d", &addProductPrice)
+			if addProductPrice == -1 {
+				break
+			}
 			product.productPrice = addProductPrice
-			fmt.Printf("จำนวนสินค้าที่เพิ่ม : ")
+			fmt.Printf("จำนวนสินค้าที่เพิ่ม (กด -1 เพื่อยกเลิกการซื้อ): ")
 			fmt.Scanf("%d", &addProductQuantity)
+			if addProductQuantity == -1 {
+				break
+			}
 			product.productQuantity += addProductQuantity
 			for i := 0; i < len(productList); i++ {
 				if product.productName == productList[i].productName && productList[i].productPrice == product.productPrice {

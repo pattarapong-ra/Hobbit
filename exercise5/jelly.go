747ef7645523896e9504e6d5ac1e05874c0437a9
pattarapong-ra <pattarapong.ra@tnis.com>
2023-02-21 11:26:24 +0700

    go mod

---
+++ exercise5/jelly.go
@@ -0,0 +1,20 @@
+package exercise5
+
+import "fmt"
+
+func main() {
+	var sideA, sideB, sideC int
+	fmt.Printf("input side a :")
+	fmt.Scanf("%d", &sideA)
+	fmt.Printf("input side b :")
+	fmt.Scanf("%d", &sideB)
+	fmt.Printf("input side c :")
+	fmt.Scanf("%d", &sideC)
+}
+
+func jelly(a, b, c int) int {
+	// check longest
+	//recursive
+	//end with return count
+	return a
+}
